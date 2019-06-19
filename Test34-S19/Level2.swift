7c7313d134071a6f92c362069dfa7e9eeca5ba0b
MacStudent <macstudent@130-IMAC-11.local>
2019-06-19 13:31:12 -0400

    tracking the drag

--- Test34-S19/Level2.swift
+++ Test34-S19/Level2.swift
@@ -22,31 +22,52 @@ class Level2: SKScene {
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
     }
+    var mouseStratingPosition:CGPoint = CGPoint(x:0,y:0)
+    
+    //this function gets the posisiton of tap down
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        let touch = touches.first
-        if (touch == nil) {
-            return
+        
+        
+        
+        //getting the mouse position on tap down
+        let mousePosition = touches.first?.location(in: self)
+        
+        //checking the x position
+        print(mousePosition!.x)
+        
+        //make orange only when user clicks on tree
+        
+        //first check on what sprite the use clicks
+        let spriteTouched = self.atPoint(mousePosition!)
+        
+        //now check if the touched sprite is tree
+        if(spriteTouched.name == "tree")
+        {
+            //getting the initial mouse position only when person taps the tree
+            self.mouseStratingPosition = mousePosition!
         }
-        
-        let location = touch!.location(in:self)
-        let node = self.atPoint(location)
-        
-        
-        
-        
-        // MARK: Switch Levels
-        
-        if (node.name == "nextLevelButton") {
-            let scene = SKScene(fileNamed:"Level3")
-            if (scene == nil) {
-                print("Error loading level")
-                return
-            }
-            else {
-                scene!.scaleMode = .aspectFill
-                view?.presentScene(scene!)
-            }
+        else{
+            self.mouseStratingPosition = CGPoint(x:0,y:0)
         }
         
     }
+    
+    //getting the mouse position on tap release
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        //getting the mouse position on tap release
+        let mousePosition = touches.first?.location(in: self)
+        
+        if(self.mouseStratingPosition.x != 0 && self.mouseStratingPosition.y != 0)
+        {
+            //gettin difference between strating and ending
+            let diffX = mousePosition!.x - mouseStratingPosition.x
+            let diffY = mousePosition!.y - mouseStratingPosition.y
+            
+            //make orange at mouse position and throw it in correct direction
+            
+         
+            
+        }}
+
 }
